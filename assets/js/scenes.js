e8309ae32a92509d10e064b996838d6990d276ca
ermon07 <ermon1995@gmail.com>
2026-06-09 19:58:58 +0800

    Updated Room Test and Arrow Position

--- assets/js/scenes.js
+++ assets/js/scenes.js
@@ -1,27 +1,27 @@
 const scenes = {
     room1: {
-        title: "Room 1",
+        title: "Stair",
         panorama: "assets/panoramas/room1.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 180,
                 type: "scene",
-                text: "Go to Room 2",
+                // text: "Go to Room 2",
                 sceneId: "room2"
             }
         ]
     },
 
     room1b: {
-        title: "Room 1",
+        title: "Stair",
         panorama: "assets/panoramas/room1.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 180,
                 type: "scene",
-                text: "Go to Room 2",
+                // text: "Go to Room 2",
                 sceneId: "room2"
             }
         ],
@@ -31,21 +31,21 @@ const scenes = {
     },
 
     room2: {
-        title: "Room 2",
+        title: "Information Desk",
         panorama: "assets/panoramas/room2.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 110,
                 type: "scene",
-                text: "Back to Room 1",
+                // text: "Back to Room 1",
                 sceneId: "room1b"
             },
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 180,
                 type: "scene",
-                text: "Go to Room 3",
+                // text: "Go to Room 3",
                 sceneId: "room3"
             }
         ],
@@ -54,21 +54,21 @@ const scenes = {
     },
 
       room2b: {
-        title: "Room 2",
+        title: "Information Desk",
         panorama: "assets/panoramas/room2.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 110,
                 type: "scene",
-                text: "Back to Room 1",
+                // text: "Back to Room 1",
                 sceneId: "room1b"
             },
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 180,
                 type: "scene",
-                text: "Go to Room 3",
+                // text: "Go to Room 3",
                 sceneId: "room3"
             }
         ],
@@ -78,42 +78,42 @@ const scenes = {
     },
 
     room3: {
-        title: "Room 3",
+        title: "Bookshelves",
         panorama: "assets/panoramas/room3.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 0,
                 type: "scene",
-                text: "Back to Room 2",
+                // text: "Back to Room 2",
                 sceneId: "room2b"
             },
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 180,
                 type: "scene",
-                text: "Go to Room 4",
+                // text: "Go to Room 4",
                 sceneId: "room4"
             }
         ]
     },
 
     room3b: {
-        title: "Room 3",
+        title: "Bookshelves",
         panorama: "assets/panoramas/room3.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 0,
                 type: "scene",
-                text: "Back to Room 2",
+                // text: "Back to Room 2",
                 sceneId: "room2b"
             },
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: 180,
                 type: "scene",
-                text: "Go to Room 4",
+                // text: "Go to Room 4",
                 sceneId: "room4"
             }
         ],
@@ -124,14 +124,14 @@ const scenes = {
     },
 
      room4: {
-        title: "Room 4",
+        title: "Bookshelves",
         panorama: "assets/panoramas/room4.jpg",
         hotSpots: [
             {
-                pitch: -2,
+                pitch: -20,
                 yaw: -10,
                 type: "scene",
-                text: "Back to Room 3",
+                // text: "Back to Room 3",
                 sceneId: "room3b"
             }
         ]
